88faa3f3abf238edb5832ff02e5e6293d3634cd4
Sebstian Pietrykowski <s_pietrykowski@outlook.com>
2021-12-10 23:04:08 +0100

    dodanie podstawienia wstecznego w backsubst.c

--- lmp9/trunk/src/backsubst.c
+++ lmp9/trunk/src/backsubst.c
@@ -7,6 +7,30 @@
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 
+	int i, j;
+        int n = mat->r; /* stopien macierzy */
+        double suma = 0.0;
+
+        if( (mat->c != n) || (x->r != n) || (x->c != 1) ||
+                        (b->r != n) || (b->c != 1) ) {
+                fprintf( stderr, "Błędny rozmiar macierzy
+                                w podstawieniu wstecznym\n" );
+                return 2;
+        }
+
+        x->data[n-1][0] = b->data[n-1][0] / mat->data[n-1][n-1];
+        for( i = n-2; i >= 0; i-- ) {
+                suma = 0.0;
+                for( j = i+1; j < n; j++ )
+                        suma += mat->data[i][j] * x->data[j][0];
+                if( mat->data[i][i] == 0.0 ) {
+                /* dzielenie przez 0 */
+                        fprintf( stderr, "Błąd w podstawieniu wstecznym:
+                                        próba dzielenie przez 0\n" );
+                        return 1;
+                }
+                x->data[i][0] = ( b->data[i][0] - suma ) / mat->data[i][i];
+        }
 
 	return 0;
 
